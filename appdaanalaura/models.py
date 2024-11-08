6e6accd3536377fc60a017a743cee98600340bd0
analauramonte <analauramonte@gmail.com>
2024-11-08 00:57:44 +0000

    TEstando

--- appdaanalaura/models.py
+++ appdaanalaura/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class Movie (models.Model):
     title = models.CharField(max_length=50)
-    description = models.TextField() 
+    description = models.TextField()
+
 
 
 
